9449c739998c6b3fccb097275e6b41ae539c07c9
Oleg Mkhamadiev <mkhamadiev@mail.ru>
2023-09-20 14:36:55 +0500

    feat: начать верстать компонент BurgerIngredients

--- src/components/burger-ingredients/burger-ingredient.jsx
+++ src/components/burger-ingredients/burger-ingredient.jsx
@@ -1,6 +1,15 @@
+import { Tab } from '../tab/tab';
 import styles from './burger-ingredients.module.css';
 
 
 function BurgerIngredients () {
-    
+    return (
+        <main className={styles.container}>
+            <h1 className="text text_type_main-large pt-10">Соберите бургер</h1>
+            <Tab />
+            <section className={`custom-scroll pt-10 ${styles.ingredientsContainer}`}>
+                <h2 className="text text_type_main-medium"></h2>
+            </section>
+        </main>
+    )
 }
